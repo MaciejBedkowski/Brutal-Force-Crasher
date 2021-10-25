9af41ae24363ef6475db2d847cf51f6dbd8f428f
Maciej Będkowski <mathiass222@gmail.com>
2021-10-25 09:38:17 +0200

    Finally project

---
+++ Brutal Force Crasher/Brutal Force Crasher/Program.cs
@@ -0,0 +1,121 @@
+﻿using SharpCompress.Archives;
+using SharpCompress.Common;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Brutal_Force_Crasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Sign> signs = new List<Sign>();
+            string path = "MarciejB.7z";
+            string password;
+            string tmp;
+            bool passwordSearch = true;
+            bool addSign = false;
+            int signCount = 0;
+            int tryPassword = 0;
+            Stopwatch stopWatch = new Stopwatch();
+
+            signs.Add(new Sign());
+            password = signs[0].sign;
+
+            stopWatch.Start();
+
+            while (passwordSearch)
+            {
+                signCount = signs.Count() - 1;
+
+                ////  CREATE PASSWORD  /////
+                tmp = "";
+                foreach (var item in signs)
+                {
+                    tmp += item.sign;
+                }
+                password = tmp;
+                ////////////////////////////
+
+                try
+                {
+                    using (var archive = SharpCompress.Archives.SevenZip.SevenZipArchive.Open(path, new SharpCompress.Readers.ReaderOptions() { Password = password, LookForHeader = false }))
+                    {
+                        foreach (var entry in archive.Entries.Where(entry => !entry.IsDirectory))
+                        {
+                            entry.WriteToDirectory("temp", new ExtractionOptions()
+                            {
+                                ExtractFullPath = true,
+                                Overwrite = true
+                            });
+                        }
+                    }
+                    //Success
+                    passwordSearch = false;
+                    stopWatch.Stop();
+                }
+                catch (SharpCompress.Common.InvalidFormatException ex)
+                {
+                    Console.WriteLine("Something wrong: " + password);
+                }
+                catch (SharpCompress.Common.PasswordProtectedException ex)
+                {
+                    Console.WriteLine("Password wrong: " + password);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Password wrong: " + password);
+                }
+
+                signs[signCount].nextSign();
+
+                if (signs[signCount].ifIsTheLastSign())
+                {
+                    if (signCount == 0)
+                    {
+                        signs[signCount].resetSign();
+                        signs.Insert(0, new Sign());
+                    }
+                    else
+                    {
+                        signs[signCount].resetSign();
+                        addSign = true;
+                    }
+                }
+
+                for (int i = signCount - 1; i >= 0; i--)
+                {
+                    if (addSign)
+                    {
+                        if (signs[i].ifIsTheLastSign())
+                        {
+                            signs[i].resetSign();
+                        }
+                        else
+                        {
+                            signs[i].nextSign();
+                            addSign = false;
+                        }
+                    }
+                    if (addSign && i == 0)
+                    {
+                        signs.Insert(0, new Sign());
+                        addSign = false;
+                    }
+                }
+
+                tryPassword++;
+
+            }
+
+            TimeSpan ts = stopWatch.Elapsed;
+
+            Console.WriteLine($"{tryPassword} - password guesses");
+            Console.WriteLine($"Time the password was cracked:{ts}");
+            Console.WriteLine($"Hasło to: {password}");
+        }
+    }
+}
---
+++ Brutal Force Crasher/Brutal Force Crasher/Sign.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brutal_Force_Crasher
+{
+    public class Sign
+    {
+        private string passwordSigns = "qwertyuiopasdfghjklzxcvbnm0123456789*";
+        public string sign = "q";
+        public int positionSign = 1;
+
+        public void nextSign()
+        {
+            sign = passwordSigns[positionSign].ToString();
+            positionSign++;
+        }
+
+        public void resetSign()
+        {
+            sign = "q";
+            positionSign = 1;
+        }
+
+        public bool ifIsTheLastSign()
+        {
+            if(sign == "9")
+                return true;
+            return false;
+        }
+    }
+}
